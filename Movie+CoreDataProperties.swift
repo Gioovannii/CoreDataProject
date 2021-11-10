2aef6f02a86e7d1d5f492a0b36c14cbb114c53ae
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-10 13:25:00 +0800

    create some NSManagedObjectModel object subclasses

---
+++ Movie+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Movie+CoreDataProperties.swift
+//  CoreDataProject
+//
+//  Created by Giovanni Gaffé on 2021/11/10.
+//
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Movie> {
+        return NSFetchRequest<Movie>(entityName: "Movie")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var director: String?ç
+    @NSManaged public var year: Int16
+
+    public var wrappedValue: String {
+        return title ?? "Unknown title"
+    }
+}
+
+extension Movie : Identifiable {
+
+}
