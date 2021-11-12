9671013a2a50a983855b007c1f969dfe142e60d5
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-12 15:03:16 +0800

    Update launch file

--- CoreDataProject/CoreDataProjectApp.swift
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -13,7 +13,7 @@ struct CoreDataProjectApp: App {
 
     var body: some Scene {
         WindowGroup {
-            RelationshipView()
+            DynamicFilteringView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
